e27d22e88b4e98d2b6378d1fe97f1e340179ae6a
Chenyu <920901835@qq.com>
2017-10-29 16:29:27 +0800

    添加结果对话框

---
+++ src/ui/RankDialog.java
@@ -0,0 +1,77 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.RowSorter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+public class RankDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private DefaultTableModel model;
+	private String []title = {"玩家", "点数", "起始赌注", "倍率", "胜负"};
+    private Object [][]data;
+
+	public RankDialog() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 414, 208);
+		contentPanel.add(scrollPane);
+		table = new JTable();
+		model = new DefaultTableModel(data,title){
+            @Override
+            public Class getColumnClass(int column) {
+                Class returnValue;
+                if ((column >= 0) && (column < getColumnCount())) {
+                    returnValue = getValueAt(0, column).getClass();
+                } else {
+                    returnValue = Object.class;
+                }
+                return returnValue;
+            }
+        };
+		table.setModel(model);
+		
+		//启用排序
+		RowSorter<TableModel> sorter = new TableRowSorter<>(model);
+        table.setRowSorter(sorter);
+		scrollPane.setViewportView(table);
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				RankDialog.this.dispose();
+			}
+		});
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+	}
+	
+    /**
+     * 向Rank List添加一条结果
+     */
+    public void addResult(){
+        Object []row = {model.getRowCount()+1,"1","2","2","1"};
+        model.addRow(row);
+        table.validate();
+    }
+}
